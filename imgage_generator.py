b13cad55c7239eb54d9ede17b59cec01e6669834
Pedro Jaber <pedrojaberc@gmail.com>
2024-02-04 14:15:23 -0300

    code customization

--- imgage_generator.py
+++ imgage_generator.py
@@ -4,10 +4,17 @@ from PIL import Image as img
 
 import time
 
+# Image Size
+size_h = 1000
+size_w = 1000
+
+# Image Count
+img_max = 1000
 ctt = 0
 
+
 while True:
-    image_array = np.random.randint(0, 256, size=(1000, 1000, 3), dtype=np.uint8)
+    image_array = np.random.randint(0, 256, size=(size_h, size_w, 3), dtype=np.uint8)
 
     # Create a PIL Image object from the NumPy array
     image = img.fromarray(image_array, 'RGB')
@@ -17,7 +24,7 @@ while True:
         old_img = img.open("imgs/output_image.jpg")
 
         ctt += 1
-        if ctt > 1000:
+        if ctt > img_max:
             ctt = 1
         print(ctt)
         old_img.save(f"imgs/old/output_image_{ctt}.jpg")
